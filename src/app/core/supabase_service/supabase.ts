676dfb6b0084b74bb5512be5dc0831fa7f6211df
Davi Lessa <davilessa.br@gmail.com>
2025-08-15 19:07:16 +0200

    supabase initial logic is implemented

---
+++ src/app/core/supabase_service/supabase.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { createClient, type SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment';
+import { Post } from '../../features/posts/interface/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SupabaseService {
+  private client: SupabaseClient = createClient(
+    environment.supabaseUrl,
+    environment.supabaseAnonKey
+  );
+
+  get db() {
+    return this.client;
+  }
+
+  async getPosts(): Promise<Post[]> {
+		const { data, error } = await this.client
+			.from('posts')
+			.select('*');
+		console.log('Supabase posts:', data, error);
+		if (error) throw error;
+		return data;
+  	}
+}
---
+++ src/app/features/posts/interface/post.ts
@@ -0,0 +1,10 @@
+export interface Post {
+    id: number;
+    title: string;
+    description: string;
+    funrating: number;
+    image_url: string;
+    created_by: string;
+    created_at: string;
+
+}
